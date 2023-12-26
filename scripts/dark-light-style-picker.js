dcacc8a837f6bfaffaf520bb7dcb340c014c6e10
lehrmanaidins <lehrmanaidin+github.com@gmail.com>
2023-12-25 23:02:31 -0500

    First Commit

---
+++ scripts/dark-light-style-picker.js
@@ -0,0 +1,13 @@
+
+lightSchemeIcon = document.querySelector('link#light-scheme-icon');
+darkSchemeIcon = document.querySelector('link#dark-scheme-icon');
+
+function onUpdate() {
+  if (matcher.matches) {
+    lightSchemeIcon.remove();
+    document.head.append(darkSchemeIcon);
+  } else {
+    document.head.append(lightSchemeIcon);
+    darkSchemeIcon.remove();
+  }
+}
